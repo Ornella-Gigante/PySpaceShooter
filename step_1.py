fe5a1f105626eddd490cb18339736df5367f2b4a
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-02-28 16:20:05 +0100

    Comments in code explanatory

--- step_1.py
+++ step_1.py
@@ -1,10 +1,13 @@
 import pygame, sys, os, random , math 
 from pygame.locals import *
 
+
+# INIT GAME 
+
 pygame.init()
 fps = pygame.time.Clock()
 
-# COLORS
+# COLORS USING RGB VALUES 
 
 WHITE = (255, 255, 255)
 RED = (255, 0,0) 
@@ -13,7 +16,7 @@ BLACK = (0,0,0)
 
 
 
-# GLOBALS
+# GLOBAL VARIABLES THAT IM GONNA USE 
 
 WIDTH = 800
 HEIGHT = 600 
@@ -21,38 +24,40 @@ time = 0
 
 
 
-# CANVAS DECLARATION
+# GAME WINDOW 
 
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Asteroids')
 
 
-# LOAD IMAGES 
+# LOAD IMAGES FROM DIRECTORY 
 
 bg = pygame.image.load(os.path.join('images', 'bg.jpg'))
 debris = pygame.image.load(os.path.join('images','debris2_brown.png'))
 
 
-# DRAW GAME FUNCTION
+# FUNCTIONS TO DRAW GAME ELEMENTS 
 
 def draw(canvas):
     canvas.fill(BLACK)
     canvas.blit(bg,(0,0))
-    canvas.blit(debris(time*3,0))
+    canvas.blit(debris,(time*.3,0))
     canvas.blit(debris,(time*.3-WIDTH,0))
 
-# HANDLE INPUT FUNCTION 
-
+# FUNCTION TO HANDLE USER (MOUSE, KEYBOARD, ETC)
 def handle_input():
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+# FUNCTION TO UPDATE THE SCREEN 
+
 def update_screen():
     pygame.display.update()
-    fps.tick(60)
+    fps.tick(60) 
     
+
 # ASTEROIDS GAME LOOP
 
 while True: 
