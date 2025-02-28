3f980cd2017166b4ca19a96862827c50429b7a3d
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-02-28 15:32:12 +0100

    setting up of basic structure of the game

--- step_1.py
+++ step_1.py
@@ -1,2 +1,41 @@
 import pygame, sys, os, random , math 
 from pygame.locals import *
+
+pygame.init()
+fps = pygame.time.Clock()
+
+# COLORS
+
+WHITE = (255, 255, 255)
+RED = (255, 0,0) 
+GREEN = (0, 255, 0) 
+BLACK = (0,0,0)
+
+
+
+# GLOBALS
+
+WIDTH = 800
+HEIGHT = 600 
+time = 0
+
+
+
+# CANVAS DECLARATION
+
+window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption('Asteroids')
+
+
+# LOAD IMAGES 
+
+bg = pygame.image.load(os.path.join('images', 'bg.jpg'))
+debris = pygame.image.load(os.path.join('images','debris2_brown.png'))
+
+# ASTEROIDS GAME LOOP
+
+while True: 
+    draw(window)
+    handle_input()
+    # game_logic() 
+    update_screen()
